0c272fc9b47794af19c8448be9060ddb4062ad26
Jon Perkins <captainperk81@gmail.com>
2018-11-03 08:50:29 -0400

    Creating an Asynchronous Result Filter

--- Filters/Infrastructure/ViewResultDetailsAttribute.cs
+++ Filters/Infrastructure/ViewResultDetailsAttribute.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Filters.Infrastructure
 {
@@ -24,6 +25,26 @@ namespace Filters.Infrastructure
                 ViewName = "Message",
                 ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary()) { Model = dictionary}
             };
+        }
+
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            var dictionary = new Dictionary<string, string> { ["Result Type"] = context.Result.GetType().Name };
+
+            if (context.Result is ViewResult viewResult)
+            {
+                dictionary["View Name"] = viewResult.ViewName;
+                dictionary["Model Type"] = viewResult.Model.GetType().Name;
+                dictionary["Model Data"] = viewResult.Model.ToString();
+            }
+
+            context.Result = new ViewResult
+            {
+                ViewName = "Message",
+                ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary()) { Model = dictionary }
+            };
+
+            await next();
         }
     }
 }
