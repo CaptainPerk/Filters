79ae5a4cc3343f6b1673e5105d47cff8663cf310
Jon Perkins <captainperk81@gmail.com>
2018-11-03 09:54:46 -0400

    Diagnostics Filter

--- Filters/Infrastructure/DiagnosticsFilter.cs
+++ Filters/Infrastructure/DiagnosticsFilter.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Filters.Infrastructure
 {
-    public class DiagnosticsFilter
+    public class DiagnosticsFilter : IAsyncResultFilter
     {
+        private readonly IFilterDiagnostics _filterDiagnostics;
+
+        public DiagnosticsFilter(IFilterDiagnostics filterDiagnostics)
+        {
+            _filterDiagnostics = filterDiagnostics;
+        }
+
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            await next();
+
+            foreach (var message in _filterDiagnostics.Messages)
+            {
+                byte[] bytes = Encoding.ASCII.GetBytes($"<div>{message}</div>");
+                await context.HttpContext.Response.Body.WriteAsync(bytes, 0, bytes.Length);
+            }
+        }
     }
 }
