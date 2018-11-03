95a9abf7ce93ef8297b853a343b6ceade74f92e3
Jon Perkins <captainperk81@gmail.com>
2018-11-03 09:53:49 -0400

    Creating Filters with Dependencies

---
+++ Filters/Infrastructure/TimeFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Filters.Infrastructure
+{
+    public class TimeFilter : IAsyncActionFilter, IAsyncResultFilter
+    {
+        private readonly IFilterDiagnostics _filterDiagnostics;
+        private Stopwatch stopwatch;
+
+        public TimeFilter(IFilterDiagnostics filterDiagnostics)
+        {
+            _filterDiagnostics = filterDiagnostics;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            stopwatch = Stopwatch.StartNew();
+            await next();
+            _filterDiagnostics.AddMessage($"Action Time: {stopwatch.Elapsed.TotalMilliseconds}");
+        }
+
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            await next();
+            stopwatch.Stop();
+            _filterDiagnostics.AddMessage($"Result Time: {stopwatch.Elapsed.TotalMilliseconds}");
+        }
+    }
+}
